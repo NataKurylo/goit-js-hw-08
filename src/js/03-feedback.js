f9411bbc51a3e0722d7e232c9dce1d07a68ca571
Nataliia Kurylo <kurilo.nata17@gmail.com>
2023-03-27 02:08:59 +0300

    Changes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,28 @@
 import throttle from 'lodash.throttle';
-const emailEl = document.querySelector('input');
-const messageEl = document.querySelector('textarea');
-const formEl = document.querySelector('.feedback-form');
-   
-const userFeedback = {};
 
-formEl.addEventListener('input', throttle(handlerInput, 500));
-function handlerInput(event) {
-    userFeedback[event.target.name] = event.target.value;
-    localStorage.setItem('feedback-form-state', JSON.stringify(userFeedback));
-}
+const formEl = document.querySelector('.feedback-form');
   
-const userData = JSON.parse(localStorage.getItem('feedback-form-state'));
-if (userData !== null) {
-    emailEl.value = userData.email;
-    messageEl.value = userData.message;
-} 
+let userFeedback = {};
+let savedUserData = localStorage.getItem('feedback-form-state');
+    
+if (savedUserData !== null) {
+    userFeedback = JSON.parse(savedUserData);
+    for (const key in userFeedback) {
+        formEl.elements[key].value = userFeedback[key];
+        // console.log(formEl.elements[key].value);
+        // console.log(userFeedback[key]);
+    }
+}
+
+formEl.addEventListener('input', throttle(handleInput, 500));
+function handleInput(event) {
+    userFeedback[event.target.name] = event.target.value.trim();
+    localStorage.setItem('feedback-form-state', JSON.stringify(userFeedback));
+    // console.log(userFeedback[event.target.name]);
+    // console.log(event.target.value)
+}
+// console.log(userFeedback);
+// console.log(savedUserData);
 
 formEl.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
@@ -28,8 +35,10 @@ function handleSubmit(event) {
         alert("Please fill in all the fields!");
         return
     }
-
+    
     console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
     event.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');
+    localStorage.removeItem('feedback-form-state', JSON.stringify(userFeedback));
+    userFeedback = {};
+    formEl.removeEventListener('input', throttle(handleInput, 500));
 }
